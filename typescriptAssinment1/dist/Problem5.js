8aecc784b94af3cc2cda7502e782fc4cfa81fff7
Md Sujon Mia <sujon258549@gmail.com>
2024-11-08 17:55:48 +0600

    final-1

--- typescriptAssinment1/dist/Problem5.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-{
-    // Write a generic function getProperty that takes an object and a property name as arguments and returns the property value. Add a constraint to ensure the property name exists on the object.
-    const getProperty = (obj, kye) => {
-        return obj[kye];
-    };
-    const person = { name: "Alice", age: 30 };
-    // console.log(getProperty(person, "name"))
-}
